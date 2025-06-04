a76dae20fbe6efb3f4f75ae96280819b0e0d7ee6
Johnathon Russell <johnathon@Johnathons-MacBook-Pro.local>
2025-06-03 21:31:26 -0400

    Add .gitignore

---
+++ dnum.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file dnum.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-06-03
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include <type_traits>
+#include <concepts>
+#include <vector>
+#include <variant>
+
+
+template<typename T>
+class dnum {
+    
+
+public:
+
+    dnum() : (){
+
+    }
+
+private:
+
+};
